72f4997cfcea3aeb4a2ec217ec435f22aa855e95
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-05-18 14:51:07 +0200

    Server: refactor nodejs Server. Cleanup

---
+++ Server/api/api.js
@@ -0,0 +1,45 @@
+var https          = require('https'),
+    express        = require('express');
+
+var router = express.Router();
+var lines = require('../Scripts/lines.json');
+var linesKeys = Object.keys(lines);
+console.log(linesKeys)
+
+router.get('/question/beginning', function(req, res) {
+    var line = linesKeys[Math.floor(Math.random() * linesKeys.length)];
+    var directions = lines[line];
+    var station = Object.keys(directions)[0];
+    var question = {};
+    console.log(station)
+    question["question"] = "Czy " + line + ' zaczyna kurs od przystanku "' + directions[station][0][1] + '"?';
+    question["answer"]   = "1"
+    res.send({"result": question});
+});
+
+router.post('/auth', function (req, res){
+    var access_token = String(req.body.access_token)
+
+    var options = {
+        host: 'graph.facebook.com',
+        port: 443,
+        path: '/me' + '?access_token=' + access_token,
+        method: 'GET'
+    };
+    var buffer = '';
+    https.get(options, function(result){
+        result.setEncoding('utf8');
+        result.on('data', function(chunk){
+            buffer += chunk;
+        });
+
+        result.on('end', function(){
+            console.log(buffer);
+            res.send({"id": JSON.parse(buffer)['id'], "access_token": access_token});
+        });
+    });
+
+});
+
+
+module.exports = router;
